4d45c40fbef1d281cec16937a9d856f7a65ae2ef
Q23RF <97279294+Q23RF@users.noreply.github.com>
2022-10-22 14:39:55 +0000

    log in with google

--- main.py
+++ main.py
@@ -1,62 +1,31 @@
 from flask import Flask, render_template, request, flash
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import os
 
 app = Flask(__name__)
-app.secret_key = os.environ['secret_key']
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-login_manager.login_message = '請先登入！'
 
-class User(UserMixin):
-    pass
-
-@login_manager.user_loader
-def user_loader(使用者):
-    if 使用者 not in users:
-        return
-
-    user = User()
-    user.id = 使用者
-    return user
-
-@login_manager.request_loader
-def request_loader(request):
-    使用者 = request.form.get('user_id')
-    if 使用者 not in users:
-        return
-
-    user = User()
-    user.id = 使用者
-    user.is_authenticated = request.form['password'] == users[使用者]['password']
-
-    return user
-
-users = {'Me': {'password': 'myself'}}
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	if request.method == 'GET':
-		return render_template("login.html")
-
-	使用者 = request.form['user_id']
-	if (使用者 in users) and (request.form['password'] == users[使用者]['password']):
-		user = User()
-		user.id = 使用者
-		login_user(user)
-		flash('welcome!')
-		return redirect(url_for('home'))
-	flash('login failed!')
-	return render_template('login.html')
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/home')
-@login_required
-def home():
-	return "home"
+@app.route('/google_sign_in', methods=['POST'])
+def google_sign_in():
+    token = request.json['id_token']
+    
+    try:
+        id_info = id_token.verify_oauth2_token(
+            token,
+            requests.Request(),
+            GOOGLE_OAUTH2_CLIENT_ID
+        )
+        if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise ValueError('Wrong issuer.')
+    except ValueError:
+        # Invalid token
+        raise ValueError('Invalid token')
+ 
+    print('登入成功')
+    return jsonify({}), 200
+
 
 app.run(host='0.0.0.0', port=81)
