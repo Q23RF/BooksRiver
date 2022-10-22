0dcf68f2d85453d77cda10b0ebe6e79a046f6cfc
Q23RF <97279294+Q23RF@users.noreply.github.com>
2022-10-22 17:37:24 +0000

    log in with google (unstable)

--- main.py
+++ main.py
@@ -1,31 +1,76 @@
-from flask import Flask, render_template, request, flash
 import os
+import pathlib
+
+import requests
+from flask import Flask, session, abort, redirect, request, url_for, render_template
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "q43w56erutjkgmfds35465t"
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, 'client_secret.json')
 
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="https://BooksRiver.elsie094081.repl.co/callback"
+)
+
+def google_login_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return redirect(url_for('login'))
+        else:
+            return function()
+
+    return wrapper
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/google_sign_in', methods=['POST'])
-def google_sign_in():
-    token = request.json['id_token']
+@app.route('/login')
+def login():
+    authorization_url, state = flow.authorization_url()
+    session['state'] = state
+    return redirect(authorization_url)
+
+@app.route('/callback')
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session['state'] == request.args['state']:
+        abort(500)
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
     
-    try:
-        id_info = id_token.verify_oauth2_token(
-            token,
-            requests.Request(),
-            GOOGLE_OAUTH2_CLIENT_ID
-        )
-        if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-            raise ValueError('Wrong issuer.')
-    except ValueError:
-        # Invalid token
-        raise ValueError('Invalid token')
- 
-    print('登入成功')
-    return jsonify({}), 200
+    session['google_id'] = id_info.get('sub')
+    session['name'] = id_info.get('name')
+    return redirect('/home')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/home')
+@google_login_required
+def home():
+    return render_template('home.html', username=session['name'])
 
 
-app.run(host='0.0.0.0', port=81)
+if __name__ == '__main__':
+    app.run(port=8040, host='0.0.0.0', debug=False)
